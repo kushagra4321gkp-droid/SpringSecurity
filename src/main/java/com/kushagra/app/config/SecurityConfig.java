84bf51460f94a0f70ea110af2aebce8e9f6e9722
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-02-09 20:25:51 +0530

    Disabling CSRFToken

--- src/main/java/com/kushagra/app/config/SecurityConfig.java
+++ src/main/java/com/kushagra/app/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.kushagra.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +14,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        //using lambda
+
+        http.csrf(customizer -> customizer.disable());
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        //http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return http.build();
     }
 
