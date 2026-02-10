529d7e2ead2b813084a95f92b64e9645e7a33a46
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-02-10 23:20:42 +0530

    Authentication Provider

--- src/main/java/com/kushagra/app/config/SecurityConfig.java
+++ src/main/java/com/kushagra/app/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.kushagra.app.config;
 
+import com.mysql.cj.protocol.AuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,6 +21,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    public AuthenticationProvider authProvider(){
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService();
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+
+        return provider;
+
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,25 +50,27 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(){
 
-        UserDetails admin = User
-                .withDefaultPasswordEncoder()
-                .username("Kushagra")
-                .password("kush@123savita")
-                .roles("ADMIN")
-                .build();
-
-        UserDetails user = User
-                .withDefaultPasswordEncoder()
-                .username("user")
-                .password("1234")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
-
-    }
+//Static Values
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//
+//        UserDetails admin = User
+//                .withDefaultPasswordEncoder()
+//                .username("Kushagra")
+//                .password("kush@123savita")
+//                .roles("ADMIN")
+//                .build();
+//
+//        UserDetails user = User
+//                .withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("1234")
+//                .roles("USER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+//
+//    }
 
 }
