735976bdd19f6c904388f96a1280c561c2b273d3
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-02-13 20:13:38 +0530

    BCrypt Encoding for User Registration

--- src/main/java/com/kushagra/app/service/UserService.java
+++ src/main/java/com/kushagra/app/service/UserService.java
@@ -3,6 +3,7 @@ package com.kushagra.app.service;
 import com.kushagra.app.dao.UserRapo;
 import com.kushagra.app.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,11 @@ public class UserService {
     @Autowired
     private UserRapo rapo;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     public User saveUser(User user){
+        user.setPasswordd(encoder.encode(user.getPasswordd()));
+        System.out.println(user.getPasswordd());
         return rapo.save(user);
     }
 
